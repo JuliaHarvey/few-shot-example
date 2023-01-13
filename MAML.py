ef66f031cbb31540156ddc320e3f6bc43cd7f5f2
Julia Harvey <33609557+JuliaHarvey@users.noreply.github.com>
2023-01-12 18:34:48 -0700

    added documentation for better understanding of paper to proceed with implementation

--- MAML.py
+++ MAML.py
@@ -1,23 +1,25 @@
 # Model-Agnostic Meta-Learning
 
+
 class MAML():
-    def __init__(self, alpha, beta, K, theta = 1.0):
+    def __init__(self, alpha, beta, K, dist, theta = 1.0):
         self.C = None
         self.alpha = alpha
         self.beta = beta
         self.theta = theta #randomly initialize parameters (or can import pretrained parameters)
         self.K = K
+        self.dist = dist #distribution over tasks
     
-    def train(num_episodes = 50):
+    def train(self, num_episodes = 50):
         curr_episode = 1
         while curr_episode != num_episodes:
             #sample batch
             sample_batch = []
             for batch in sample_batch:
-                evaluate(batch)
+                self.evaluate(batch)
                 #update theta
     
-    def evaluate(batch):
+    def evaluate(self, batch):
         examples = []
         for image in examples:
-            C.evaluate(image)
+            self.C.evaluate(image)
